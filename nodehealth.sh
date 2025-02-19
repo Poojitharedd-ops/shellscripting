31830184db49ecf775113406aafd3c24cb69ac1c
poojitha <saidreddy5@gmail.com>
2025-02-19 22:12:50 +0530

    prints node health

---
+++ nodehealth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##################metadatainformn
+
+#Author: Poojitha
+#Date: 19-02-2025
+#this scrippt outputs the nodehealth
+#to check the disk space
+set -x  #debug mode #instead of writing echo statements put this
+
+df -h
+#to print the memory
+
+free -g 
+#to print the resources
+
+nproc
