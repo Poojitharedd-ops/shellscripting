455a4212624fde7e521ec984461c452c93162200
poojitha <poojithadspr@gmail.com>
2025-02-20 14:32:50 +0530

    if-else

---
+++ ifelse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ 10 -gt 7 ];
+then
+	echo "x is greater"
+else
+	echo "y is greater".
+fi
--- nodehealth.sh
+++ nodehealth.sh
@@ -6,7 +6,8 @@
 #this scrippt outputs the nodehealth
 #to check the disk space
 set -x  #debug mode #instead of writing echo statements put this
-
+set -e   #exists thescript if there is an error but dont exit when there is a pipefaail so give pipefail command as well
+set -o pipefail
 df -h
 #to print the memory
 
@@ -14,3 +15,8 @@ free -g
 #to print the resources
 
 nproc
+ps -ef
+ 
+#curl command retrieves theinformation from the internet
+#curl urlof logfile | grep error ## now u can get error logs
+#wget will download log file but curl can display directly
